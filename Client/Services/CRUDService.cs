8f4d2bf497e483d6a071e6e498d1b093fdcb74a3
Randel Ramirez <randelramirez1@gmail.com>
2021-01-15 11:53:59 +0800

    Code clean up

--- Client/Services/CRUDService.cs
+++ Client/Services/CRUDService.cs
@@ -1,6 +1,5 @@
 ﻿using Core;
 using Core.ViewModels;
-using Microsoft.AspNetCore.JsonPatch;
 using Newtonsoft.Json;
 using Polly;
 using Polly.Fallback;
@@ -10,7 +9,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
@@ -189,108 +187,5 @@ namespace Client.Services
             Console.WriteLine($"Name: {createdContact.Name}, Address: {createdContact.Address}");
             return createdContact;
         }
-
-        //public async Task UpdateContact()
-        //{
-        //    // create a new contact and then update it
-        //    var contactToUpdateViewModel = await CreateContact();
-        //    Console.WriteLine($"Name before update: {contactToUpdateViewModel.Name}, Address: {contactToUpdateViewModel.Address}");
-
-        //    // assign the id of the retrieved contact
-        //    var contactToUpdate = new Contact()
-        //    {
-        //        Id = contactToUpdateViewModel.Id,
-        //        Name = $"Updated contact name! {DateTimeOffset.UtcNow}",
-        //        Address = "Updated Address"
-        //    };
-
-        //    var serializedContactToUpdate = JsonConvert.SerializeObject(contactToUpdate);
-
-        //    var request = new HttpRequestMessage(HttpMethod.Put,
-        //        $"api/contacts/{contactToUpdate.Id}");
-        //    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        //    request.Content = new StringContent(serializedContactToUpdate);
-        //    request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-        //    var response = await httpClient.SendAsync(request);
-        //    response.EnsureSuccessStatusCode();
-
-        //    Console.WriteLine($"StatusCode: {(int)response.StatusCode} {response.StatusCode}");
-        //    var content = await response.Content.ReadAsStringAsync();
-        //    //Console.WriteLine(content); content is empty 
-
-        //    var updatedContact = await GetContact(contactToUpdateViewModel.Id);
-        //    Console.WriteLine($"Name after update: {updatedContact.Name}, Address after update: {updatedContact.Address}");
-        //}
-
-        private async Task DeleteContact()
-        {
-            // Create a new one
-            var contact = await CreateContact();
-            Console.WriteLine($"Deleting contact... Id: {contact.Id}, Name: {contact.Name}, Address: {contact.Address}");
-            // Delete the newly created
-
-            var request = new HttpRequestMessage(HttpMethod.Delete,
-                $"api/contacts/{contact.Id}");
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            var response = await httpClient.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-
-            Console.WriteLine($"StatusCode: {(int)response.StatusCode} {response.StatusCode}");
-            var content = await response.Content.ReadAsStringAsync();
-            //Console.WriteLine(content); content is empty 
-        }
-
-        //public async Task PatchContactThroughHttpRequestMessage()
-        //{
-        //    // create a contact and then update it
-        //    var contact = await CreateContact();
-
-        //    var patchDoc = new JsonPatchDocument<Contact>();
-
-        //    patchDoc.Replace(c => c.Name, "Updated Name with Patch");
-        //    patchDoc.Remove(c => c.Address);
-
-        //    var serializedChangeSet = JsonConvert.SerializeObject(patchDoc);
-
-        //    var request = new HttpRequestMessage(HttpMethod.Patch,
-        //        $"api/contacts/{contact.Id}");
-        //    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        //    request.Content = new StringContent(serializedChangeSet);
-        //    request.Content.Headers.ContentType =
-        //        new MediaTypeHeaderValue("application/json-patch+json");
-
-        //    var response = await httpClient.SendAsync(request);
-        //    response.EnsureSuccessStatusCode();
-
-        //    Console.WriteLine($"StatusCode: {(int)response.StatusCode} {response.StatusCode}");
-        //    var patchedContact = await GetContact(contact.Id);
-        //    Console.WriteLine($"Name after patch: {patchedContact.Name}, Address after patch: {patchedContact.Address}");
-        //}
-
-        //public async Task PatchContact()
-        //{
-        //    // create a contact and then update it
-        //    var contact = await CreateContact();
-
-        //    var patchDoc = new JsonPatchDocument<Contact>();
-        //    patchDoc.Replace(c => c.Name, "Updated Name");
-        //    patchDoc.Remove(c => c.Address);
-
-        //    var response = await httpClient.PatchAsync(
-        //       $"api/contacts/{contact.Id}",
-        //       new StringContent(
-        //           JsonConvert.SerializeObject(patchDoc),
-        //          Encoding.UTF8,
-        //           "application/json-patch+json"));
-
-        //    response.EnsureSuccessStatusCode();
-        //    //var content = await response.Content.ReadAsStringAsync(); // content is emptu, we are returning 204
-
-        //    Console.WriteLine($"StatusCode: {(int)response.StatusCode} {response.StatusCode}");
-        //    var patchedContact = await GetContact(contact.Id);
-        //    Console.WriteLine($"Name after patch: {patchedContact.Name}, Address after patch: {patchedContact.Address}");
-        //}
     }
 }
