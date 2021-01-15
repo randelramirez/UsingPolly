db22731da4a925ab0dec6a366efc44be4f2071ca
Randel Ramirez <randelramirez1@gmail.com>
2021-01-15 14:57:28 +0800

    Implemented Dependency Injection for retreiving Policy Holder

---
+++ Client/PolicyHolder.cs
@@ -0,0 +1,39 @@
+﻿using Polly;
+using Polly.Retry;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Client
+{
+    public class PolicyHolder
+    {
+        public AsyncRetryPolicy<HttpResponseMessage> HttpRetryPolicy { get; }
+
+        public AsyncRetryPolicy HttpClientTimeoutException { get; }
+
+        public PolicyHolder()
+        {
+            // Wait and Retry with delegate
+            HttpRetryPolicy =
+                Policy.HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                    .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt) / 2), onRetry: (httpResponseMessage, retryCount) =>
+                    {
+                        // Log result
+                        Console.WriteLine(httpResponseMessage.Result);
+
+                        Console.WriteLine($"Retrying...");
+                    });
+
+            HttpClientTimeoutException = Policy.Handle<HttpRequestException>()
+                .WaitAndRetryAsync(1, retryAttempt => TimeSpan.FromSeconds(retryAttempt),
+                    onRetry: (exception, timespan) =>
+                    {
+                        string message = exception.Message;
+                        // log the message.
+                    }
+                );
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -43,59 +43,11 @@ namespace Client
                 builder.AddConsole();
                 builder.AddDebug();
             });
-            // register IService on our container with a 
-            // scoped lifetime
 
-            // For the CRUD demos
-            serviceCollection.AddScoped<IService, WaitRetryDelegateService>();
+            serviceCollection.AddSingleton<PolicyHolder>();
+            //serviceCollection.AddScoped<IService, WaitRetryDelegateService>();
+            serviceCollection.AddScoped<IService, PolicyHolderFromDIService>();
 
-            // For stream demos
-            //serviceCollection.AddScoped<IService, StreamService>();
-
-            // For the HttpClientFactory demos
-            //serviceCollection.AddScoped<IService, HttpClientFactoryManagementService>();
-
-            // Using a named client
-            serviceCollection.AddHttpClient("ContactsClient", client =>
-            {
-                client.BaseAddress = new Uri("https://localhost:44354/");
-                client.Timeout = new TimeSpan(0, 0, 30);
-                client.DefaultRequestHeaders.Clear();
-            })
-            .ConfigurePrimaryHttpMessageHandler(handler => new HttpClientHandler()
-            {
-                AutomaticDecompression = System.Net.DecompressionMethods.GZip
-            });
-
-            serviceCollection.AddHttpClient("ContactsClientCustomHandler", client =>
-            {
-                client.BaseAddress = new Uri("https://localhost:44354/");
-                client.Timeout = new TimeSpan(0, 0, 30);
-                client.DefaultRequestHeaders.Clear();
-            })
-            // We use 20 seconds at least for this demo so that we can differtiate timeout and cancelled exceptions 
-            // (the httpclient have 30 seconds before timeout and then it throws a cancelled exception)
-           .AddHttpMessageHandler(handler => new TimeOutDelegatingHandler(TimeSpan.FromSeconds(20)))
-           .AddHttpMessageHandler(handler => new RetryPolicyDelegatingHandler(2))
-           .ConfigurePrimaryHttpMessageHandler(handler =>
-                new HttpClientHandler()
-                {
-                    AutomaticDecompression = System.Net.DecompressionMethods.GZip
-                });
-
-            // using Typed client
-            serviceCollection.AddHttpClient<ContactsClient>()
-            .ConfigurePrimaryHttpMessageHandler(handler =>
-                 new HttpClientHandler()
-                 {
-                     AutomaticDecompression = System.Net.DecompressionMethods.GZip
-                 });
-
-            // For the dealing with errors and faults demos
-            //serviceCollection.AddScoped<IService, ErrorHandlingService>();
-
-            // For the custom http handlers demos
-            //serviceCollection.AddScoped<IService, HttpCustomMessageHandlerService>();
         }
     }
 }
