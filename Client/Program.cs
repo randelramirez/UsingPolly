4256e812a87465d749148b43ebd16c552bbe75f1
Randel Ramirez <randelramirez1@gmail.com>
2021-01-20 14:39:31 +0800

    renamed servicecollection to services

--- Client/Program.cs
+++ Client/Program.cs
@@ -35,25 +35,25 @@ namespace Client
             Console.ReadKey();
         }
 
-        private static void ConfigureServices(IServiceCollection serviceCollection)
+        private static void ConfigureServices(IServiceCollection services)
         {
-            serviceCollection.AddLogging(builder =>
+            services.AddLogging(builder =>
             {
                 builder.AddConsole();
                 builder.AddDebug();
             });
 
             // simple way to inject HttpClient, only suitable for demo application like this(for Production apps use HttpClientFactory)
-            serviceCollection.AddSingleton<HttpClient>(new HttpClient());
+            services.AddSingleton<HttpClient>(new HttpClient());
             
-            serviceCollection.AddSingleton<IPolicyHolder>(new PolicyHolder());
-            serviceCollection.AddSingleton<PolicyRegistry>(PolicyRegistryFactory.GetRegistry());
+            services.AddSingleton<IPolicyHolder>(new PolicyHolder());
+            services.AddSingleton<PolicyRegistry>(PolicyRegistryFactory.GetRegistry());
 
             // Services to run for testing Polly
-            //serviceCollection.AddScoped<IService, WaitRetryDelegateTimeoutService>();
-            //serviceCollection.AddScoped<IService, PolicyHolderFromDIService>();
-            //serviceCollection.AddScoped<IService, UsingPolicyRegistryService>();
-            serviceCollection.AddScoped<IService, UsingContextService>();
+            //services.AddScoped<IService, WaitRetryDelegateTimeoutService>();
+            //services.AddScoped<IService, PolicyHolderFromDIService>();
+            //services.AddScoped<IService, UsingPolicyRegistryService>();
+            services.AddScoped<IService, UsingContextService>();
         }
     }
 }
